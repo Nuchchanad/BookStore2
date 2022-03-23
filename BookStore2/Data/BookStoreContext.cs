b1bc6e3e80e75e4c3718068e55c71b0a95d21a81
Admin <Admin@DESKTOP-I0RCIO2>
2022-03-23 17:25:57 +0700

    Add project files.

---
+++ BookStore2/Data/BookStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStore2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore2.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
